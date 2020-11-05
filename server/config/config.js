09b4172be0bff6148c11aa3d4df4a448e51e47b1
Jonathan Robles <jrobles@simetrical.com.mx>
2020-11-05 00:30:26 -0600

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -26,3 +26,8 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
 // Base de Datos
 //=====================
 process.env.URL_DB = process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/cafe';
+
+//=====================
+// Google Client ID
+//=====================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '369814469174-v1h9okm5uves1m5377oscckrmbi6u7ck.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
 
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
 
 const bodyParser = require('body-parser');
 
@@ -11,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json());
+
+// habilitar carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // Configuracion global de rutas
 app.use(require('./routes/index'));
